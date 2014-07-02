a4059ed2424b580a0e20ad5a02be1d4937fc1e3f
Chai Feng <chaifeng@chaifeng.com>
2014-07-02 10:55:56 +0800

    通过了 username 为空白字符串的测试

--- src/main/java/com/baldurtech/scaling/octo/ironman/MemberService.java
+++ src/main/java/com/baldurtech/scaling/octo/ironman/MemberService.java
@@ -9,7 +9,7 @@ public class MemberService {
     }
 
     public Member save(Member member) {
-        if(member.getUsername() != null && member.getUsername().length() > 0) {
+        if(member.getUsername() != null && member.getUsername().trim().length() > 0) {
             return memberDao.save(member);
         }
         return member;
