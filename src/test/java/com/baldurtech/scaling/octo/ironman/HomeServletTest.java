b6dab3f750440aa8b19be89e06cddafdee963005
Chai Feng <chaifeng@chaifeng.com>
2014-07-04 13:51:45 +0800

    重构 HomeServlet

--- src/test/java/com/baldurtech/scaling/octo/ironman/HomeServletTest.java
+++ src/test/java/com/baldurtech/scaling/octo/ironman/HomeServletTest.java
@@ -4,32 +4,30 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class HomeServletTest extends ScalingOctoIronmanTestCase {
+    MockRequestForm form = new MockRequestForm();
+    MockMemberService memberService = new MockMemberService();
+    HomeServlet homeServlet = new HomeServlet(memberService);
+
     public void test_member保存成功后要返回list页面() {
         Member expectedMember = createMemberWithIdAndUsername(1L, "Tom");
-
-        MockMemberService memberService = new MockMemberService();
         memberService.saveShouldReturnMember = expectedMember;
-        MockRequestForm form = new MockRequestForm();
         form.set("action", "save");
 
-        HomeServlet homeServlet = new HomeServlet(memberService);
 
         Map dataModel = homeServlet.doAction(form);
+
 
         assertEquals("list", dataModel.get("redirectTo"));
     }
 
     public void test_member保存失败后要返回编辑页面() {
         Member expectedMember = createMemberWithUsername(" Tom ");
-
-        MockMemberService memberService = new MockMemberService();
         memberService.saveShouldReturnMember = expectedMember;
-        MockRequestForm form = new MockRequestForm();
         form.set("action", "save");
 
-        HomeServlet homeServlet = new HomeServlet(memberService);
 
         Map dataModel = homeServlet.doAction(form);
+
 
         assertEquals("edit", dataModel.get("forward"));
         assertEquals(expectedMember, dataModel.get("member"));
